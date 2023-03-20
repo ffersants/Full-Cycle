fc1a3e8f26f1dbf5f88e1a232a8b6ba496b11595
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2023-03-20 02:09:38 -0300

    inciada implementação dos métodos do struct e implementando seus testes

---
+++ Arquitetura Hexagonal/application/product.go
@@ -0,0 +1,86 @@
+package application
+
+import (
+	"errors"
+
+	"github.com/asaskevich/govalidator"
+)
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type Product struct {
+	//apos a especificação do tipo do campo da sruct, temos o que é chamado de tags ``.
+	//essas tags são basicamente notations que definem se o valor atribuído ao campo em questão é válido.
+	//por exemplo, para o campo ID, só o consideramos como válido se o seu valor for do tipo uuidv4
+	ID     string  `valid:"uuidv4"`
+	Name   string  `valid:"required"`
+	Price  float64 `valid:"float,optional"`
+	Status string  `valid:"required"`
+}
+
+func (p *Product) IsValid() (bool, error) {
+	if p.Status == "" {
+		p.Status = DISABLED
+	}
+
+	if p.Status != ENABLED && p.Status != DISABLED {
+		return false, errors.New("O status deve ser disabled ou enabled")
+	}
+
+	if p.Price < 0 {
+		return false, errors.New("O preço deve ser maior que 0")
+	}
+
+	_, err := govalidator.ValidateStruct(p)
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (p *Product) Enable() error {
+	if p.Price > 0 {
+		p.Status = ENABLED
+		return nil
+	}
+	return errors.New("O valor do produto deve ser informado")
+}
+
+func (p *Product) Disable() error {
+	if p.Price == 0 {
+		p.Status = DISABLED
+		return nil
+	}
+	return errors.New("O valor do produto deve ser 0")
+}
+
+func (p *Product) GetID() string {
+	return p.ID
+}
+
+func (p *Product) GetName() string {
+	return p.Name
+}
+
+func (p *Product) GetStatus() string {
+	return p.Status
+}
+
+func (p *Product) GetPrice() float64 {
+	return p.Price
+}
---
+++ Arquitetura Hexagonal/application/product_test.go
@@ -0,0 +1,54 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/ffersants/full-cycle/application"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "O valor do produto deve ser informado", err.Error())
+
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "O valor do produto deve ser 0", err.Error())
+
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "invalid"
+	_, err = product.IsValid()
+	require.Equal(t, "O status deve ser disabled ou enabled", err.Error())
+}
