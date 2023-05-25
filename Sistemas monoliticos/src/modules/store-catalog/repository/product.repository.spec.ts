801820c682c8623f942c19b8053de3f9812b3d47
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2023-05-24 23:02:40 -0300

    implementado product repository e seu test

---
+++ Sistemas monoliticos/src/modules/store-catalog/repository/product.repository.spec.ts
@@ -0,0 +1,42 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "./product.model";
+import ProductRepository from "./product.repository";
+
+describe("ProductRepository", () => {
+    let sequelize: Sequelize;
+	beforeEach(async () => {
+		sequelize = new Sequelize({
+			dialect: "sqlite",
+			storage: ":memory:",
+			logging: false,
+			sync: { force: true },
+		});
+
+		await sequelize.addModels([ProductModel]);
+		await sequelize.sync();
+	});
+
+	afterEach(async () => {
+		await sequelize.close();
+	});
+    
+    it("should find all products", async() => {
+        await ProductModel.create({
+            id: "1",
+            name: "product 1",
+            description: "dsc 1",
+            salesPrice: 500
+        })
+        await ProductModel.create({
+            id: "2",
+            name: "product 2",
+            description: "dsc 2",
+            salesPrice: 500
+        })
+
+        const productRepo = new ProductRepository()
+        const result = await productRepo.findAll()
+
+        expect(result).toHaveLength(2)
+    })
+})
---
+++ Sistemas monoliticos/src/modules/store-catalog/repository/product.repository.ts
@@ -0,0 +1,28 @@
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Product from "../domain/product.entity";
+import ProductGateway from "../gateway/product.gateway";
+import ProductModel from "./product.model";
+
+export default class ProductRepository implements ProductGateway {
+	async findAll(): Promise<Product[]> {
+		return (await ProductModel.findAll()).map(
+			(product) =>
+				new Product({
+					id: new Id(product.id),
+					description: product.description,
+					name: product.name,
+					salesPrice: product.salesPrice,
+				})
+		);
+	}
+	async find(id: string): Promise<Product> {
+		const product = await ProductModel.findOne({ where: { id } });
+		if (!product) throw new Error("Product not found");
+		return new Product({
+			id: new Id(product.id),
+			description: product.description,
+			name: product.name,
+			salesPrice: product.salesPrice,
+		});
+	}
+}
