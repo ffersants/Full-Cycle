529e47ccf978db9101b4297391c67bc6706fd0f0
ditec134897 <ditec134897@iMac-de-ditec134897.local>
2023-05-29 13:36:40 -0300

    iniciando modulo payment

---
+++ Sistemas monoliticos/src/modules/payment/usecase/process-payment/process-payment.usecase.ts
@@ -0,0 +1,8 @@
+import UseCaseInterface from "../../../@shared/usecase/add-product/use-case.interface";
+
+export default class ProcessPaymentUsecase implements UseCaseInterface {
+    execute(input: any): Promise<any> {
+        throw new Error("Method not implemented.");
+    }
+
+}
