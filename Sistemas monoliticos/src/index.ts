c66a5f569f023457432bbed2d0d369384c415efa
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2023-06-16 00:03:02 -0300

    implementado get invoice

--- Sistemas monoliticos/src/index.ts
+++ Sistemas monoliticos/src/index.ts
@@ -1,38 +1,68 @@
-import { validate } from 'class-validator';
-import express from 'express'
-import CreateProductModelValidation from "./app/validations/create-product-model-validation"
-import ProductAdmFacadeFactory from './modules/product-adm/factory/facade.factory';
-import ClientFacadeFactory from './modules/client-adm/factory/facade.factory';
-import CreateClientModelValidation from './app/validations/create-client-model-validation';
-const app = express()
+import { validate } from "class-validator";
+import express from "express";
+import CreateProductModelValidation from "./app/validations/create-product-model-validation";
+import ProductAdmFacadeFactory from "./modules/product-adm/factory/facade.factory";
+import ClientFacadeFactory from "./modules/client-adm/factory/facade.factory";
+import CreateClientModelValidation from "./app/validations/create-client-model-validation";
+import InvoiceFacadeFactory from "./modules/invoice/factory/facade.factory";
+const app = express();
 
-app.post("products", async(req, res) => {
-    const productFacade = ProductAdmFacadeFactory.create()
+app.post("products", async (req, res) => {
+	try {
+		const productFacade = ProductAdmFacadeFactory.create();
 
-    const product = req.body;
-    const validationErrors = await validate(new CreateProductModelValidation(), product);
-  
-    if (validationErrors.length > 0) {
-      // Handle validation errors
-      res.status(400).json({ errors: validationErrors });
-    } else {
-      productFacade.addProduct(product)
-    }
-})
+		const product = req.body;
+		const validationErrors = await validate(
+			new CreateProductModelValidation(),
+			product
+		);
 
-app.post("clients", async(req, res) => {
-  const clientFacade = ClientFacadeFactory.create()
+		if (validationErrors.length > 0) {
+			// Handle validation errors
+			res.status(400).json({ errors: validationErrors });
+		} else {
+			await productFacade.addProduct(product);
+			res.status(200).send();
+		}
+	} catch (e) {
+		res.status(500).send("Algo deu errado. Favor tentar novamente");
+	}
+});
 
-  const client = req.body;
-  const validationErrors = await validate(new CreateClientModelValidation(), client);
+app.post("clients", async (req, res) => {
+	try {
+		const clientFacade = ClientFacadeFactory.create();
 
-  if (validationErrors.length > 0) {
-    // Handle validation errors
-    res.status(400).json({ errors: validationErrors });
-  } else {
-    clientFacade.add(client)
-  }
-})
+		const client = req.body;
+		const validationErrors = await validate(
+			new CreateClientModelValidation(),
+			client
+		);
 
-//TODO: implementar post de checkout e facade do checkout. 
+		if (validationErrors.length > 0) {
+			// Handle validation errors
+			res.status(400).json({ errors: validationErrors });
+		} else {
+			await clientFacade.add(client);
+			res.status(200).send();
+		}
+	} catch (e) {
+		res.status(500).send("Algo deu errado. Favor tentar novamente");
+	}
+});
+
+app.get("invoice/:id", async (req, res) => {
+	try {
+		const invoiceToGet = req.params.id;
+
+		const invoiceFacade = InvoiceFacadeFactory.create();
+
+		const result = await invoiceFacade.find({ id: invoiceToGet });
+		res.send(result);
+	} catch (e) {
+		res.status(500).send("Algo deu errado. Favor tentar novamente");
+	}
+});
+
+//TODO: implementar endpoint POST/checkout
 //TODO: implementar testes dos endpoints com supertest
